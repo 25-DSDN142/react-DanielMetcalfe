933bf7582463d41fbbfec93fb12ce353425beed5
DanielMetcalfe <metcaldani@myvuw.ac.nz>
2025-10-13 22:29:32 +1300

    Adding in static grass and animated clouds
    Adding in static grass in the foreground to make the flowers look like they belong. This was hand drawn in krita.
    
    Adding in rolling clouds animation in the background. I precomposed 3 clouds together as one image to save processing power and so I only needed to load one image. I then used the X position of the cloud image to animate it, mapping the frameRate to the X position to get it to move. I used two instances of the image, as one instance look too sparse, The second cloud is staggered so that it starts when the first lot reach halfway through the sketch.
    
    I wanted to add some horizontal movement to the scene and give the scene some life to match the growth interaction of the flowers and hand.
    
    The clouds were drawn in krita and then comped into one image in photoshop.

--- 1Draw_hands.js
+++ 1Draw_hands.js
@@ -1,15 +1,23 @@
 // ----=  HANDS  =----
 /* load images here */
-let frameImages = []; // Array to store the 10 images
-let previousDistance = 0;
+let frameImages = []; //array for the images loaded into the flower animation
+let previousDistance = 0; //previous distance is the last recorded hand position, the last know distance between the wrist and middle finger tip. This is used to make sure mapping the hand to the flower ends up smooth
+let grass; //static grass image
+let clouds;// animated clouds image
+
+let cloudX = -1280; //first cloud image starting point off left of the screen
+let cloudX2 = 0 ;//repetition of cloud image, the starting point for this one, starts halfway through first versions travel across the screen
 
 function prepareInteraction() {
   //bgImage = loadImage('/images/background.png');
   
   // Load all 24 frames
   for (let i = 1; i <= 24; i++) {//for loop for loading all images automatically
-    frameImages[i] = loadImage('/images/' + i + '.webp');
+    frameImages[i] = loadImage('/images/' + i + '.webp'); //webp for smaller files
   }
+
+  grass = loadImage('/images/grass.png'); //loading the static grass overlay
+  clouds = loadImage('/images/clouds.png'); //loading the image for cloud animation
 }
 
 function drawInteraction(faces, hands) {
@@ -17,6 +25,23 @@ function drawInteraction(faces, hands) {
   let bgClr = color(95,215,255); //blue
   
   background(bgClr);//test bg
+  
+  let cloudSpeed = 20 * frameRate(); //controls the speed of the cloud animation,being used to control the x position, using the frameRate function and times it by 20 to get slower rate as it will be divided by this value next. Using frameRate instead of frameCount so that the animation is always the same
+  
+  cloudX += (2560) / (cloudSpeed); //the first run of clouds, this animates the clouds by mapping the x position to the frameRate, 1280 is the width of the sketch and the image size, 2560 as that is the distance needed to have the 1280 image go fully of the left and right side of sketch
+  cloudX2 += (2560) / (cloudSpeed); //second run of clouds X position, how the movement is created
+
+  if (cloudX > 1280) { //resetting the animation to start on the left again after it goes off the right side of sketch
+    cloudX = -1280;
+  }
+
+  if (cloudX2 > 1280) { //resetting the animation to start on the left again after it goes off the right side of sketch
+    cloudX2 = -1280;
+  }
+
+  image(clouds, cloudX, 0, 1280, 960); //imaging the clouds
+  image(clouds, cloudX2, 0, 1280, 960);
+
 
   // hands part
   // for loop to capture if there is more than one hand on the screen. This applies the same process to all hands.
@@ -43,7 +68,7 @@ function drawInteraction(faces, hands) {
       previousDistance = distance; // starts previous distance at whatever the distance between the wrist and middle finger is, essentially what your hand position is
     }
     
-    let smoothedDistance = lerp(previousDistance, distance, 0.3); //making the animation smoother by essentially taking a point in between the previous distance and new distance of the points on the hand, this ensures that the frame that is displayed is close enough to the previous one that is doesn't get jumpy, this is repeated for the next frame etc so it always goes forwards or backwards in frames but it does it slower than realtime movement. the 0.3 value makes it slower to transition to the next as it keeps the point closer to the previous distance than the new distance.
+    let smoothedDistance = lerp(previousDistance, distance, 0.3); //making the animation smoother by essentially taking a point in between the previous distance and new distance of the points on the hand, this ensures that the frame that is displayed is close enough to the previous one that is doesn't get jumpy, this is repeated for the next frame etc so it always goes forwards or backwards in frames but it does it slower than realtime movement. the 0.3 value makes it slower to transition to the next as it keeps the point closer to the previous distance than the new distance. coding train lerp video helped with this
     previousDistance = smoothedDistance; //setting the previous distance as this new inbetween value for smooth
     
   
@@ -53,7 +78,7 @@ function drawInteraction(faces, hands) {
     
     let frameNum = map(smoothedDistance, minDistance, maxDistance, 1, 24); //mapping frame number to distance using smoothed value to keep smooth animation
     
-    frameNum = constrain(frameNum, 1, 24); // Keep it inside number of frames for later if neccessary
+    frameNum = constrain(frameNum, 1, 24); // Keep it inside number of frames so that it never breaks, eg if someones hand is really big or really small and goes over the min or max 
     frameNum = round(frameNum); // Make it an integer, ensures that it always plays a frame as the frames are number 1 through 24
     
     
@@ -64,28 +89,26 @@ function drawInteraction(faces, hands) {
       pop();
     }
 
+
+
+    
     // drawPoints(hand)
 
-    // fingerPuppet(indexFingerTipX, indexFingerTipY);
-
-    // chameleonHandPuppet(hand)
+   
 
     /*
     Stop drawing on the hands here
     */
   }
+
+  
+
+  image(grass, 0, 0, width, height);//imaging the static grass overlay
   // You can make addtional elements here, but keep the hand drawing inside the for loop. 
   //------------------------------------------------------
 }
 
-function fingerPuppet(x, y) {
-  fill(255, 38, 219) // pink
-  ellipse(x, y, 100, 20)
-  ellipse(x, y, 20, 100)
 
-  fill(255, 252, 48) // yellow
-  ellipse(x, y, 20) // draw center 
-}
 
 function pinchCircle(hand) { // adapted from https://editor.p5js.org/ml5/sketches/DNbSiIYKB
   // Find the index finger tip and thumb tip
