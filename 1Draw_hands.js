b55e5d16e73ce251fb6515ce242aa0d06d565750
DanielMetcalfe <metcaldani@myvuw.ac.nz>
2025-10-14 23:26:13 +1300

    Adding in a butterfly that is mapped to face position
    Mapping a butterfly animation to follow where the face is on the X axis by mapping the face to the X location of the image. I chose not to map the Y location of the butterfly to the face as it got too chaotic and distracted from the rest of the scene. Adding it to only the X axis makes it subtle enough to not disrupt the scene.
    
    Using the same code I used for the smoothing the hand mapping to make the animation smoother when it follows the face. Using the lerp Function.
    
    Also making it so that the butterfly faces the same way the face moves, eg if the face moves to the right, the butterfly faces right and moves to the right. This made it look more natural, and made it feel more interactive.

--- 1Draw_hands.js
+++ 1Draw_hands.js
@@ -1,25 +1,45 @@
 // ----=  HANDS  =----
 /* load images here */
+
+let previousDistance = 0; //previous distance is the last recorded right hand position, the last know distance between the wrist and middle finger tip. This is used to make sure mapping the hand to the flower ends up smooth
+let previousDistanceLeft = 0; //left hand version of above variable
+
 let frameImages = []; //array for the images loaded into the flower animation
-let previousDistance = 0; //previous distance is the last recorded hand position, the last know distance between the wrist and middle finger tip. This is used to make sure mapping the hand to the flower ends up smooth
-let previousDistanceLeft = 0;
+let butterflyFrames = []; //array for the butterfly animation frames
+
 let grass; //static grass image
 let clouds;// animated clouds image
-let texture;
-let texture2;
+let texture; //texture overlay image
+let texture2;//texture overlay image
 
+//sun and moon colour switches
 let Night= 0; //day night bg lerpColor switch, day =0, 1= night
-let Night2= 0;
+let Night2= 0; //day night sun and moon lerpColor switch, day =sun/yellow, 1= moon/grey
 
+//clouds
 let cloudX = -1280; //first cloud image starting point off left of the screen
 let cloudX2 = 0 ;//repetition of cloud image, the starting point for this one, starts halfway through first versions travel across the screen
+
+//butterfly 
+let previousFaceX = 0; //how to track where the face was previously to make animation and switch in butterfly direction smoother
+let smoothedFaceX = 0; //making the butterfly animation more smooth using lerp
+let butterflyFacingRight = false;//variable for changing direction of butterfly to match facemovement
+let directionCounter = 0; //counter to make changes from left to right direction of the butterfly smoother
+let directionThreshold = 5; //the threshold of when to make the switch in direction for the butterfly, 5 felt fast enough that it responded to the face moving but not too fast that it caused flickering back and forth with subtle face movement
+let butterflyFrameNumber = 0; //variable for imaging a frame from the array for the butterfly animation
+let butterflyFrameCount = 0; //counter used to make the frameRate of the butterfly animation play at 24fps but keep sketch frame Rate at 60fps to allow smooth animations coded in p5
 
 function prepareInteraction() {
   //bgImage = loadImage('/images/background.png');
   
-  // Load all 24 frames
+  // Load all 24 frames of the flower animation automatically, loading into array
+  
   for (let i = 1; i <= 24; i++) {//for loop for loading all images automatically
     frameImages[i] = loadImage('/images/' + i + '.webp'); //webp for smaller files
+  }
+
+  for (let i = 1; i <= 10; i++) {//loading the frames for the butterfly in a similiar way to make it automatic
+    butterflyFrames[i] = loadImage('/images/butterfly_' + i + '.webp'); 
   }
 
   grass = loadImage('/images/grass.png'); //loading the static grass overlay
@@ -27,6 +47,7 @@ function prepareInteraction() {
   texture = loadImage('/images/riso1.png'); //loading texture overlay layers
   texture2 = loadImage('/images/riso3.png');
 }
+
 
 function drawInteraction(faces, hands) {
 
@@ -37,11 +58,9 @@ function drawInteraction(faces, hands) {
   let yellow= color(248,255,68); //yellow,sun
   let grey= color(220); //light grey, moon
 
-
-
   let bgClr= lerpColor ( blue,navy,Night);//lerpColor for controlling day night background colours, controlled by the left hand poses, fist = day, palm open  night
   
-  background(bgClr);//test bg
+  background(bgClr);
 
   //stars
 
@@ -68,8 +87,8 @@ function drawInteraction(faces, hands) {
  
   push();
    
-stroke(12);
-fill(circleColour);
+  stroke(12);
+  fill(circleColour);
 
   let circleX= (width *(2/3)+80); // sun/moon x location
   let circleY= height/4;// sun/moon y location
@@ -80,8 +99,7 @@ fill(circleColour);
   pop();
 
 
-
-  ///clouds
+ //clouds
   
   let cloudSpeed = 20 * frameRate(); //controls the speed of the cloud animation,being used to control the x position, using the frameRate function and times it by 20 to get slower rate as it will be divided by this value next. Using frameRate instead of frameCount so that the animation is always the same
   
@@ -89,7 +107,7 @@ fill(circleColour);
   cloudX2 += (2560) / (cloudSpeed); //second run of clouds X position, how the movement is created
 
   if (cloudX > 1280) { //resetting the animation to start on the left again after it goes off the right side of sketch
-    cloudX = -1280;
+    cloudX = -1280; //offscreen left
   }
 
   if (cloudX2 > 1280) { //resetting the animation to start on the left again after it goes off the right side of sketch
@@ -99,7 +117,67 @@ fill(circleColour);
   image(clouds, cloudX, 0, 1280, 960); //imaging the clouds
   image(clouds, cloudX2, 0, 1280, 960);
 
+  if (faces.length > 0) {
+    face = faces[0];
+    let faceCenterX = face.faceOval.centerX;
+ 
+    
+    if (previousFaceX === 0) { //if there is no face start at the location of the first face when it appears for the first time, this ensures that there is no weird jumping, the butterfly always starts where a face is located
+      previousFaceX = faceCenterX; //making is so it always assumes your current face location at start up
+      smoothedFaceX = faceCenterX; //smoothed version of face tracking/mapping to make the animation smoother
+    }
 
+    smoothedFaceX = lerp(smoothedFaceX, faceCenterX, 0.15); //using lerp to make the animation less jittery, essentially makes the next face location closer to the previous face location instead of actual face location, so that there is less of a jump between animation positions. Coding train lerp video was helpful with this
+    
+    if (faceCenterX > previousFaceX) {  // if face goes to the right of where it was previously
+      directionCounter++; ///add 1 to counter, counter is setup to make it only make changes to butterfly direction after 5 instances of the face moving in that direction to ensure smooth animations
+      if (directionCounter > directionThreshold) {//if direction counter is more than 5( the threshold I set) make it face right. This threshold makes it smoother so it only changes the butterfly direction if the face moves 5 spots in that direction, this stops any flickering back and forth that can occur from subtle face movements
+        butterflyFacingRight = true; //butterfly right facing
+        directionCounter = 0; //reset counter
+      }
+    } else if (faceCenterX < previousFaceX) { //if the face goes to the left of where it was previously
+      directionCounter--; //remove 1 from counter
+      if (directionCounter < -directionThreshold) { //if direction counter is less than 5( the threshold I set) make it face right. This threshold makes it smoother so it only changes the butterfly direction if the face moves 5 spots in that direction, this stops any flickering back and forth that can occur from subtle face movements
+        butterflyFacingRight = false; // butterfly face left
+        directionCounter = 0; //reset counter
+      }
+    }
+
+   butterflyFrameCount++; //counter to count the buttterflyFrames/time in sketch, adding 1 to it to play through frames, used to make the animation 24fps in a 60fps sketch
+    
+    if (butterflyFrameCount >= 2.5) { //way of making it 24fps in 60fps sketch. Only imaging a frame every 2.5 frameCounter instances
+    
+     butterflyFrameCount = 0; //reset the counter back to 0;
+      butterflyFrameNumber++; //add 1 to the frame number, making it play the next frame as that is the variable I am using down below to image the butterfly, happens every 2.5 frameCounts
+     
+     
+      if (butterflyFrameNumber > 10) { //resetting the frames to frame 1 once it reaches the end of the frames(10 frames)
+        butterflyFrameNumber = 1;//frame 1
+      }
+    }
+
+    if (butterflyFrameNumber === 0) {  //making sure the animation always starts at the first frame by setting it so that if there is no frame played yet it assumes frame 1, so there is no weird jumps in frames
+      butterflyFrameNumber = 1; //frame 1
+    }
+    
+    imageMode(CENTER); // making the image centered so it is centered on the face that is being tracked
+    
+    push();
+    
+    translate(smoothedFaceX, 400);// controlling the location of butterfly with translate to make it remain in the same place when flipped below
+    
+    if (!butterflyFacingRight) { // if statement to flip the butterfly so I can just load one image instead of an image for both directions
+      scale(-1, 1); //using scale to flip the x axis of the image
+    }
+    
+    image(butterflyFrames[butterflyFrameNumber], 0, 0, 175, 175); //imaging the butterfly
+    
+    pop();
+    
+    previousFaceX = faceCenterX; //setting previousFaceX variable so it is always where the current face is,updates every loop of the function to be up to date, ensuring smooth animations of the butterfly by giving the if statements above an accurate location of the face to work from
+  }
+
+  imageMode(CORNER); ///resetting the imageMode so the rest of the images aren't effected
 
   // hands part
   // for loop to capture if there is more than one hand on the screen. This applies the same process to all hands.
@@ -116,10 +194,7 @@ fill(circleColour);
     let middleFingerTipX = hand.middle_finger_tip.x;
     let middleFingerTipY = hand.middle_finger_tip.y;
 
-    /*
-    Start drawing on the hands here
-    */
-
+ 
    if (hand.handedness === "Right") { // mapping the right hand to controlling the flower growth
       
     let distance = dist(wristX, wristY, middleFingerTipX, middleFingerTipY); //how to calculate whether fist or open using distance between wrist and middle finger
@@ -173,19 +248,10 @@ fill(circleColour);
     Night2 = constrain(Night2, 0, 1); //making it so that it can't go outside 0 and 1 incase someones hand is bigger/smaller than the min max
   }
 
-    
-    // drawPoints(hand)
-
-   
-
-    /*
-    Stop drawing on the hands here
-    */
   }
 
   
   image(grass, 0, 0, width, height);//imaging the static grass overlay
-
 
   //texture overlays to give more character to sketch
   
@@ -203,74 +269,3 @@ tint(255, 80); //using tint function to overlay some textures over the top of sk
 }
 
 
-
-function pinchCircle(hand) { // adapted from https://editor.p5js.org/ml5/sketches/DNbSiIYKB
-  // Find the index finger tip and thumb tip
-  let finger = hand.index_finger_tip;
-  //let finger = hand.pinky_finger_tip;
-  let thumb = hand.thumb_tip;
-
-  // Draw circles at finger positions
-  let centerX = (finger.x + thumb.x) / 2;
-  let centerY = (finger.y + thumb.y) / 2;
-  // Calculate the pinch "distance" between finger and thumb
-  let pinch = dist(finger.x, finger.y, thumb.x, thumb.y);
-
-  // This circle's size is controlled by a "pinch" gesture
-  fill(0, 255, 0, 200);
-  stroke(0);
-  strokeWeight(2);
-  circle(centerX, centerY, pinch);
-}
-
-function chameleonHandPuppet(hand) {
-  // Find the index finger tip and thumb tip
-  // let finger = hand.index_finger_tip;
-
-  let finger = hand.middle_finger_tip; // this finger now contains the x and y infomation! you can access it by using finger.x 
-  let thumb = hand.thumb_tip;
-
-  // Draw circles at finger positions
-  let centerX = (finger.x + thumb.x) / 2;
-  let centerY = (finger.y + thumb.y) / 2;
-  // Calculate the pinch "distance" between finger and thumb
-  let pinch = dist(finger.x, finger.y, thumb.x, thumb.y);
-
-  // This circle's size is controlled by a "pinch" gesture
-  fill(0, 255, 0, 200);
-  stroke(0);
-  strokeWeight(2);
-  circle(centerX, centerY, pinch);
-
-  let indexFingerTipX = hand.index_finger_tip.x;
-  let indexFingerTipY = hand.index_finger_tip.y;
-  fill(0)
-  circle(indexFingerTipX, indexFingerTipY, 20);
-}
-
-function drawConnections(hand) {
-  // Draw the skeletal connections
-  push()
-  for (let j = 0; j < connections.length; j++) {
-    let pointAIndex = connections[j][0];
-    let pointBIndex = connections[j][1];
-    let pointA = hand.keypoints[pointAIndex];
-    let pointB = hand.keypoints[pointBIndex];
-    stroke(255, 0, 0);
-    strokeWeight(2);
-    line(pointA.x, pointA.y, pointB.x, pointB.y);
-  }
-  pop()
-}
-
-// This function draw's a dot on all the keypoints. It can be passed a whole face, or part of one. 
-function drawPoints(feature) {
-  push()
-  for (let i = 0; i < feature.keypoints.length; i++) {
-    let element = feature.keypoints[i];
-    noStroke();
-    fill(0, 255, 0);
-    circle(element.x, element.y, 10);
-  }
-  pop()
-}
